cd3a0b850658bc8a6897bab3e4aad8b19722e6c4
kaitokidi <bayonetts@hotmail.com>
2014-03-22 01:07:36 +0100

    updating main, and //ing everything -_-

--- main.cpp
+++ main.cpp
@@ -16,10 +16,30 @@ int main(){
 	sf::Vector2u windowSize(1100,700); 
 
 	//CREATE THE WINDOW
-	// for having it in fullScreen the first parameter will be sf::VideoMode::getDesktopMode().
-	// in the second parameter the L prefix denotes a wide character/string (it will be a W_char which is encoded with more bytes and so there can be accents)
+	// For having it in fullScreen the first parameter will be sf::VideoMode::getDesktopMode().
+	// In the second parameter the L prefix denotes a wide character/string 
+	// (it will be a W_char which is encoded with more bytes and so there can be accents).
 	sf::RenderWindow window(sf::VideoMode(size.x,size.y), L"1714: La resistència de l'Història", sf::Style::Resize|sf::Style::Close);
 
+
+	//MUSIC
+	/* If we want music, we will only have to do these.
+	sf::Music music;
+	if (!music.openFromFile("music/GameSong1.ogg")) 
+		std::cout << " Can't load the song " << std::endl;
+	music.play();
+	music.setPitch(1);
+	music.setLoop(true);
+	*/
+
+	//we would have to create a graphic class withe the window as a parameter so it can write on it. 
+		// the constructor of the class will be something like Class::Class(sf::RenderWindow &window) :windowGame(window) { }
+		// where windowGame will have to be the sf::RenderWindow that the class will have as a parameter. 
+		// the :windowGame(window) means: When you are using these constructor, as default initialize windowGame with the 
+		// value of 'window' (which is the parameter we are using to create the Class).
+
+	//we would have to create later a class lvl or something I still don't know how we sould do that -_- 
+		//and it's late and i have to sleep, so good night <3
 
 	//GAME LOOP
 	while(window.isOpen()){
@@ -37,7 +57,7 @@ int main(){
 				//KeyPressed event
 				case  sf::Event::KeyPressed:
 					if (event.key.code == sf::Keyboard::X) {
-						//stuff to do if X key is pressed
+						//Stuff to do if X key is pressed
 					}
 				break;
 
@@ -63,7 +83,10 @@ int main(){
 				//for more events http://www.sfml-dev.org/documentation/2.1/classsf_1_1Event.php
 				default:
 					break;
-			}
-		}
-	}
-}
+			}//switch case end
+
+		} //event loop end
+
+	} //window.open() end
+
+} //main end
