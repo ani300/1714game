9f2f215f7567c30e0a07ecdcc40db06b66c47795
kaitokidi <bayonetts@hotmail.com>
2014-03-27 21:13:05 +0100

    Player class added

---
+++ src/Player.cc
@@ -0,0 +1,13 @@
+/******************************Player.cc**********************************/
+#include "Player.h"
+
+//Constructor
+Player::Player() {
+
+
+}
+
+//Destructor
+Player::~Player() {
+
+}
---
+++ src/Player.h
@@ -0,0 +1,22 @@
+/******************************Player.h**********************************/
+#ifndef PLAYER
+#define PLAYER
+
+
+#include <SFML/Graphics.hpp>
+
+class Player {
+
+	public:
+		//Constructor
+		Player();
+
+		//Destructor
+		~Player();
+
+	private:
+
+};
+
+
+#endif
