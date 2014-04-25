7f9b0360481ee0cddb53ca64caacf05b100de4de
kaitokidi <bayonetts@hotmail.com>
2014-04-26 01:19:10 +0200

    Update Utils.h

--- src/Utils.h
+++ src/Utils.h
@@ -11,16 +11,16 @@
 #include <SFML/Graphics.hpp>
 
 enum directions {
-    dir_up, dir_down, dir_left, dir_right, dir_none
+    dir_up, dir_down, dir_left, dir_right, dir_up_right, dir_up_left, dir_down_right, dir_down_left, dir_none
 };
 
 enum mouseButtons {
     mouse_left, mouse_right, mouse_rodeta, mouse_none
 };
 
-//            		  N   S   E   W
-const int dirx[4] = { 0,  0,  1, -1};
-const int diry[4] = {-1,  1,  0,  0};
+//            		  N   S   E   W  NE  NW SE  SW NONE          
+const int dirx[9] = { 0,  0,  1, -1,  1, -1, 1, -1, 0};
+const int diry[9] = {-1,  1,  0,  0, -1, -1, 1,  1, 0};
 
 const float PlayerSpeed = 500.f;
 const sf::Time TimePerFrame = sf::seconds(1.f/60.f); // 60 fps
