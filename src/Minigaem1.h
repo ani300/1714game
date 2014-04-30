afcb66b0c62582b0f0e2e9f0315006099cab21c5
Sergi Sorigue <darkdiabl3@gmail.com>
2014-05-01 00:20:35 +0200

    added minigaem structure v2.0

--- src/Minigaem1.h
+++ src/Minigaem1.h
@@ -1,6 +1,38 @@
 #ifndef MINIGAEM1_H
 #define MINIGAEM1_H
 
+#include "Utils.h"
+#include "Logic.h"
+#include "Graphic.h"
+#include "SplashImage.h"
+#include "DrawableObject.h"
 
+
+class Minigaem1 {
+
+    public:
+        Minigaem1();
+        ~Minigaem1();
+
+        int play();
+
+    private:
+
+        void render();
+        void processEvents();
+        void update(sf::Time elapsedTime);
+        void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+        void readNextState(int& skipLines);
+
+        Logic logic;
+        directions dir;
+        mouseButtons mouseBut;
+        sf::Vector2f mouseClick;
+        sf::RenderWindow window;
+        sf::RenderTexture rTexture;
+        sf::Vector2u windowSize;
+        sf::CircleShape mPlayer;
+        std::vector<DrawableObject*> drawableObjects;
+};
 
 #endif // MINIGAEM1_H
