0d1e9cb6d6b98127699ec124e1d7c4d8d0e69950
Antoni Virós Martín <ani300@gmail.com>
2014-07-03 23:53:09 +0200

    Very CMake. Much WOW. Amaze

---
+++ src/Fight.cc
@@ -0,0 +1,17 @@
+#include "Fight.h"
+
+Fight::Fight(sf::RenderWindow *gaemWindow, sf::RenderTexture *gaemTexture) :
+    Estat(gaemWindow, gaemTexture) {
+}
+
+void Fight::update(sf::Time elapsedTime) {
+
+}
+
+void Fight::handlePlayerMouse(mouseButtons mouseBut, sf::Vector2f mouseClick) {
+
+}
+
+Fight::~Fight() {
+
+}
---
+++ src/Fight.h
@@ -0,0 +1,19 @@
+#ifndef FIGHT_H
+#define FIGHT_H
+
+#include "Utils.h"
+#include "Estat.h"
+
+class Fight : public Estat
+{
+public:
+    Fight(sf::RenderWindow *gaemWindow, sf::RenderTexture *gaemTexture);
+    ~Fight();
+    void update(sf::Time elapsedTime);
+    void handlePlayerMouse(mouseButtons mouseBut, sf::Vector2f mouseClick);
+
+private:
+
+};
+
+#endif // FIGHT_H
